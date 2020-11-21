19a4fe48fffe2102f8a91ae3f9fb5152610de2b6
Tobi <tobias.laepple92@googlemail.com>
2020-11-21 12:33:45 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out
+            .println("Hello World");
     }
 }
