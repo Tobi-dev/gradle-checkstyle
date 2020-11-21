dc0060714f46a6ff9de3d63409d40462326510f6
Tobi <tobi@Tobis-MacBook-Pro.local>
2020-11-21 11:22:01 +0100

    blaa

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Just a simple class.
+ */
+public class Main {
+    /**
+     * Just a test comment.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out
+            .println("Hello World");
+    }
+}
